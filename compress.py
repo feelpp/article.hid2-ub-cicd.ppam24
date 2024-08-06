a298fb120877cf7e43e59c3d6000c123047db759
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2024-08-06 12:51:05 +0200

    setup article.template workflow
    #1

--- compress.py
+++ compress.py
@@ -2,27 +2,37 @@ from PIL import Image
 import os
 
 
-def compress_images(directory, quality=85):
+def resize_and_compress_images(directory, max_width, max_height, quality=85):
     if not os.path.exists(directory):
         print("Directory does not exist")
         return
 
-    output_directory = os.path.join(directory, "compressed")
+    output_directory = os.path.join(directory, "processed")
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
     for filename in os.listdir(directory):
-        if filename.endswith(".jpg") or filename.endswith(".jpeg"):
-            img = Image.open(os.path.join(directory, filename))
-            img.save(os.path.join(output_directory, filename),
-                     "JPEG", quality=quality)
-        elif filename.endswith(".png"):
-            img = Image.open(os.path.join(directory, filename))
-            img.save(os.path.join(output_directory, filename),
-                     "PNG", optimize=True, quality=quality)
+        print(f"Processing {filename}")
+        if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+            img_path = os.path.join(directory, filename)
+            with Image.open(img_path) as img:
+                # Calculate the new size preserving the aspect ratio
+                img.thumbnail((max_width, max_height), Image.Resampling.LANCZOS)
+                # Save the processed image
+                if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+                    img.save(os.path.join(output_directory, filename),
+                             "JPEG", quality=quality)
+                elif filename.endswith(".png"):
+                    img.save(os.path.join(output_directory, filename),
+                             "PNG", optimize=True, quality=quality)
 
-    print(f"Images compressed and saved to {output_directory}")
+    print(f"Images resized and compressed, saved to {output_directory}")
 
 
+# Set the directory path to your image folder
 directory = 'images'
-compress_images(directory,quality=50)
+# Set the maximum width and height for the images (in pixels)
+max_width = 595  # A4 width in points (1 point = 1/72 inch)
+max_height = 842  # A4 height in points
+
+resize_and_compress_images(directory, max_width, max_height)
